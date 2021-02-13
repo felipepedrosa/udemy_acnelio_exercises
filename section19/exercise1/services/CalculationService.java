8bfa1f5660a05f28ca1cd1d9eab29efcbeab7f28
Felipe Pedrosa <felipeepedrosa@gmail.com>
2021-02-13 13:56:14 -0300

    Section 19 - Exercise 1, altered to use CollectionUtils

--- section19/exercise1/services/CalculationService.java
+++ section19/exercise1/services/CalculationService.java
@@ -1,10 +1,16 @@
 package section19.exercise1.services;
 
+import utils.src.CollectionUtils;
+
 import java.util.Collections;
 import java.util.List;
 
 public final class CalculationService {
     public static <T extends Comparable<T>> T getMax(List<T> list) {
+        if(CollectionUtils.isNullOrEmpty(list)){
+            throw new IllegalStateException("List can't be empty");
+        }
+
         return Collections.max(list);
     }
 }
